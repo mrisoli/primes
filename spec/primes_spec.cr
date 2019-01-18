d4d67e216e557b70f9a283621a3cce39b3da409b
Marcelo Risoli <celorisoli@gmail.com>
2019-01-18 18:15:39 +0100

    add upto method and tests

--- spec/primes_spec.cr
+++ spec/primes_spec.cr
@@ -3,7 +3,9 @@ require "./spec_helper"
 describe Primes do
   # TODO: Write tests
 
-  it "works" do
-    false.should eq(true)
+  describe "upto" do
+    it "finds all primes up to 10" do
+      Primes::Primes.upto(10).should eq([2,3,5,7])
+    end
   end
 end
--- src/primes.cr
+++ src/primes.cr
@@ -2,5 +2,28 @@
 module Primes
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  class Primes
+    property primes : Array(Int32)
+    def initialize(@limit : Int32)
+      @primes = sieve
+    end
+
+    # generates an array with all primes up to n
+    def self.upto(n)
+      Primes.new(n).primes
+    end
+
+    def sieve : Array(Int32)
+      indexes = Array.new(@limit, true)
+      indexes[0] = false
+      indexes[1] = false
+      (2..(Math.sqrt(@limit))).each do |i|
+        next unless indexes[i]
+        ((i**2)...@limit).step(i).each do |j|
+          indexes[j] = false
+        end
+      end
+      indexes.map_with_index{|el, i| el ? i : nil}.compact
+    end
+  end
 end
